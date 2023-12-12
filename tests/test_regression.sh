4afd6ea0fb23e949208f8e3444ede022fa890be0
Jerrick Rowe <me@jerrick.cn>
2023-12-12 19:19:24 +0800

    Fix broken CI

--- tests/test_regression.sh
+++ tests/test_regression.sh
@@ -18,9 +18,11 @@ source $PYENV/bin/activate
 $PY -m pip install -r requirements.txt
 rm -rf $SCRIPT_DIR/DSDL
 rm -rf $SCRIPT_DIR/libcanard
+rm -rf $SCRIPT_DIR/pydronecan
 
 git clone https://github.com/DroneCAN/DSDL $SCRIPT_DIR/DSDL
 git clone https://github.com/DroneCAN/libcanard $SCRIPT_DIR/libcanard
+git clone https://github.com/DroneCAN/pydronecan $SCRIPT_DIR/pydronecan
 
 echo "Testing generation with regression testing"
 $PY dronecan_dsdlc.py --output $SCRIPT_DIR/dsdl_generated $SCRIPT_DIR/DSDL/dronecan $SCRIPT_DIR/DSDL/uavcan $SCRIPT_DIR/DSDL/com $SCRIPT_DIR/DSDL/ardupilot --run-test
